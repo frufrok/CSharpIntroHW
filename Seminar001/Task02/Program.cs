93d9ab8f80b3f445f677ac8f3f12f0432d672d81
frufrok <frufrok@gmail.com>
2023-09-15 13:28:13 +0300

    Seminar001 HomeWork is done

---
+++ Seminar001/Task02/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Задача 2: Напишите программу, которая на вход
+принимает два числа и  выдаёт, какое число большее, а
+какое меньшее.
+*/
+try
+{
+    Console.WriteLine("Программа выдает максимальное из двух чисел.");
+    Console.Write("Введите целое число: a=");
+    int a = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите целое число: b=");
+    int b = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Максимальное значение: {Math.Max(a,b)}");
+}
+catch
+{
+    Console.WriteLine("Что-то пошло не так. Перезапустите программу и проверьте свой ввод.");
+}
---
+++ Seminar001/Task04/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задача 4: Напишите программу, которая принимает на
+вход три числа и выдаёт максимальное из этих чисел.
+*/
+try
+{
+    Console.WriteLine("Программа выдает максимальное из трех чисел.");
+    int[] nums = new int[3];
+    Console.Write("Введите целое число: a=");
+    nums[0] = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите целое число: b=");
+    nums[1] = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите целое число: c=");
+    nums[2] = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Максимальное значение: {nums.Max()}");
+}
+catch
+{
+    Console.WriteLine("Что-то пошло не так. Перезапустите программу и проверьте свой ввод.");
+}
