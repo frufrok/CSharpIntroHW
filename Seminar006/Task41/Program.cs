7ee5ae8780b4bacc6adf92464d10b2d9b44e45c7
Гарипов Азат Ильшатович <Garipov@ERKON.local>
2023-10-02 23:48:29 +0300

    Seminar006 folder is added.

---
+++ Seminar006/Task41/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Укажите, сколько чисел Вы хотите ввести:");
+int count = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[count];
+
+for (int i = 0; i < count; i++)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+int PositiveValuesCount(int[] array)
+{
+    PositiveValuesCount(a)
+    if (first < array.)
+    if (first < array.Length)
+    {
+        array[first] > 0 return 1 + PositiveValuesCount(array, first + 1);
+    }
+}
+
+Console.WriteLine($"Количество чисел больше 0 среди тех значений, что Вы ввели, равно {PositiveValuesCount(array)}");
